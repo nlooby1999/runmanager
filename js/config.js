aebfd977d662fd792c24a87c44ba510b08cf20ff
nlooby1999 <108774082+nlooby1999@users.noreply.github.com>
2025-11-12 12:07:50 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@
  * Keep this file out of version control (it is already ignored via .gitignore).
  */
 window.SUPABASE_CONFIG = {
-url: 'https://hmkeahingzyoymxxgrjw.supabase.co',
-anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhta2VhaGluZ3p5b3lteHhncmp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4NzA4ODEsImV4cCI6MjA3NzQ0Njg4MX0.qZl5U0wsAlpVfX4OFnusAsdANy0ewGsEEygW7JZVA7I'
+  url: 'https://oooaifnngwhpunuvjaww.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vb2FpZm5uZ3docHVudXZqYXd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4MTg3NjksImV4cCI6MjA3ODM5NDc2OX0.oplv0x641GKUuxbmC5kiH-WM9hn1e8hlAilFJyMUj3A'
 };
